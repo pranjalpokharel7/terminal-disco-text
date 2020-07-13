30e7f3b07f7ea6b2d6aa1f5d1644b12d95f3cd10
pranjalpokharel7 <pranjalpokharel007@gmail.com>
2020-07-13 11:45:55 +0545

    script-made-executable

--- randomColorGenerator.py
+++ randomColorGenerator.py
@@ -6,18 +6,18 @@ def randomColorSelector():
         Returns a random color (string) from the 8-bit terminal colors available.
     """
     colors8Bit = {
-        0 : "\u001b[30m",
-        1 : "\u001b[31m",
-        2 : "\u001b[32m",
-        3 : "\u001b[33m",
-        4 : "\u001b[34m",
-        5 : "\u001b[35m",
-        6 : "\u001b[36m",
-        7 : "\u001b[37m"
+        # "Black" : "\u001b[30m",
+        "Red" : "\u001b[31m",
+        "Green" : "\u001b[32m",
+        "Yellow" : "\u001b[33m",
+        "Blue" : "\u001b[34m",
+        "Magenta" : "\u001b[35m",
+        "Cyan" : "\u001b[36m",
+        "White" : "\u001b[37m"
     }
     
-    # generate a random color from red(1) to white(7), for black/dark terminals, black(0) is not visible
+    # generate a random color from red to white, for black/dark terminals, black(0) is not visible so commented out
     random.seed(time.time())
-    rand = random.randint(1,7) 
+    randKey = random.choice(list(colors8Bit))
 
-    return colors8Bit[rand]
+    return colors8Bit[randKey]
